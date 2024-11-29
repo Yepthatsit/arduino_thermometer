f425cbf36d8b8b850de1ca56d6b9fef885826024
Yepthatsit <126805460+Yepthatsit@users.noreply.github.com>
2024-11-29 16:24:27 +0100

    Add files via upload

--- arduino_thermometer.ino
+++ arduino_thermometer.ino
@@ -1,11 +1,18 @@
 #include <Adafruit_MAX31865.h>
+#include <LiquidCrystal_I2C.h>
 Adafruit_MAX31865 thermo = Adafruit_MAX31865(10, 11, 12, 13);
+LiquidCrystal_I2C lcd(0x27,16,2);
 #define RREF      430.0
 #define RNOMINAL  100.0
 void setup() {
   // put your setup code here, to run once:
+ 
   Serial.begin(115200);
   thermo.begin(MAX31865_2WIRE);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Arduino Thermometer");
 }
 
 void loop() {
@@ -13,19 +20,39 @@ void loop() {
   while (!Serial.available());
   String message =  Serial.readStringUntil('\n');
   if(message == "KDEG?"){
-    Serial.println(thermo.temperature(RNOMINAL, RREF) + 273.15);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    float measured_value = thermo.temperature(RNOMINAL, RREF) + 273.15;
+    lcd.print(measured_value);
+    lcd.print(" K");
+    Serial.println(measured_value);
   }
   else if(message == "CDEG?"){
-    Serial.println(thermo.temperature(RNOMINAL, RREF));
+    lcd.clear();
+    lcd.setCursor(0,0);
+    float measured_value = thermo.temperature(RNOMINAL, RREF);
+    lcd.print(measured_value);
+    lcd.print(" C");
+    Serial.println(measured_value);
   }
   else if(message == "FDEG?"){
-    Serial.println(thermo.temperature(RNOMINAL, RREF)* 1.8 + 32);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    float measured_value = thermo.temperature(RNOMINAL, RREF)* 1.8 + 32;
+    lcd.print(measured_value);
+    lcd.print(" F");
+    Serial.println(measured_value);
   }
   else if(message == "RES?"){
     uint16_t rtd = thermo.readRTD();
     float ratio = rtd;
     ratio /= 32768;
-    Serial.println(RREF*ratio,8);
+    float measured_value = RREF*ratio;
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(measured_value);
+    lcd.print(" Ohm");
+    Serial.println(measured_value);
   }
   else if(message == "*IDN?"){
     Serial.println("Arduino thermometer");
